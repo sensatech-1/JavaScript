c5bef44fa1e1644040f1969160bce93f97459a7d
SensaTech <sampsonpraise90@gmail.com>
2026-01-27 17:24:02 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10511348
+    "zoneId": 10527785
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
